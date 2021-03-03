8453ac5dd4a9d2a236ca93ca05df0534709af1a9
TELAGAMSETTI SAI TEJA <48620462+Sai999teja@users.noreply.github.com>
2021-03-03 16:23:51 +0530

    Updates

---
+++ precache-manifest.a76f4ded25f040c0a7a3c1f7c824fb96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59214ccbda1fb4db6bc33e435a90ca29",
+    "url": "/My-Portfolio.github.io/index.html"
+  },
+  {
+    "revision": "d9776995bc5af91748f7",
+    "url": "/My-Portfolio.github.io/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "3b2c48525b21fca07346",
+    "url": "/My-Portfolio.github.io/static/js/2.0b538cd4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/My-Portfolio.github.io/static/js/2.0b538cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9776995bc5af91748f7",
+    "url": "/My-Portfolio.github.io/static/js/main.569612bf.chunk.js"
+  },
+  {
+    "revision": "c40a1df14b525cc52730",
+    "url": "/My-Portfolio.github.io/static/js/runtime-main.2df8d7a9.js"
+  },
+  {
+    "revision": "2b3fff2eac61af13ba170f446fde53a6",
+    "url": "/My-Portfolio.github.io/static/media/resume.2b3fff2e.pdf"
+  },
+  {
+    "revision": "fb0c7a555fe5e1809f67d64213c86475",
+    "url": "/My-Portfolio.github.io/static/media/saipic.fb0c7a55.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.7254e722843d1f4d1e2fca19151348d3.js"
+  "/My-Portfolio.github.io/precache-manifest.a76f4ded25f040c0a7a3c1f7c824fb96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/My-Portfolio.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
