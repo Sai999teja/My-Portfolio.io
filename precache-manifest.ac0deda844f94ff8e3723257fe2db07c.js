dfd987f054fcce99ed1ccef08ae5cdeeec04bf57
TELAGAMSETTI SAI TEJA <48620462+Sai999teja@users.noreply.github.com>
2021-03-03 18:00:00 +0530

    Updates

---
+++ precache-manifest.ac0deda844f94ff8e3723257fe2db07c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7957c7399a50eba3ddb2dc370d9737",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0d2b490e20dbc3f3d42",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "3b2c48525b21fca07346",
+    "url": "/static/js/2.0b538cd4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0b538cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d2b490e20dbc3f3d42",
+    "url": "/static/js/main.7343ef74.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "2b3fff2eac61af13ba170f446fde53a6",
+    "url": "/static/media/resume.2b3fff2e.pdf"
+  },
+  {
+    "revision": "fb0c7a555fe5e1809f67d64213c86475",
+    "url": "/static/media/saipic.fb0c7a55.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.7254e722843d1f4d1e2fca19151348d3.js"
+  "/precache-manifest.ac0deda844f94ff8e3723257fe2db07c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
